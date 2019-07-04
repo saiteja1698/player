a728c44bb824621830a99cb8734d209dc0fdcba5
Saiteja <52398704+saiteja1698@users.noreply.github.com>
2019-07-04 23:06:33 +0530

    Create day.py

---
+++ day.py
@@ -0,0 +1,7 @@
+d=input("")
+if ((d=="Sunday") or (d=="Saturday")) :
+  print("yes")
+elif ((d=="Monday") or (d=="Tuesday") or (d=="Wednesday") or (d=="Thursday") or (d=="Friday")) :
+  print("no")
+else:
+    print("no")
